6712fdc157e6210d78418d9089cb1992d5acdec4
kerusu-1984 <kerusu1017@yahoo.co.jp>
2021-10-14 22:22:18 +0900

    [fix] add function

--- hex-chr.py
+++ hex-chr.py
@@ -1,6 +1,22 @@
 name="kanimusi"
-namelist = [*name]
-result = ""
-for c in [hex(ord(n)) for n in namelist]:
-    result += chr(int(c,16))
-print(result)
+
+def str_to_hex(str:str):
+    chrlist = [*str]
+    return [hex(ord(n)) for n in chrlist]
+
+def hex_to_str(hex:list):
+    result = ""
+    for c in hex:
+        result += chr(int(c,16))
+    return result
+
+def No1():
+    chrlist = [c.replace("0x","") for c in str_to_hex(name)]
+    for i in chrlist:
+        print(i,end=" ")
+
+def No2():
+    print(hex_to_str(str_to_hex(name)))
+
+No1()
+No2()
